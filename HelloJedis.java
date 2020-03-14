105351b4d3bdf4219d3647fdbaa0232ef0acb40a
DA106-11 <zero19920601@hotmail.com>
2020-03-14 15:15:31 +0800

    3rd Version

--- HelloJedis.java
+++ HelloJedis.java
@@ -2,7 +2,8 @@ package basic;
 import redis.clients.jedis.Jedis;
 
 public class HelloJedis {
-
+	// WOW
+	// master 2 
 	public static void main(String[] args) {
 		Jedis jedis = new Jedis("localhost", 6379);
 		jedis.auth("foobared");
